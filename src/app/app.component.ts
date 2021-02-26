9ff785f10a8d0a77ca03ae76201d50e23fc6cdd1
Pallavi Dhepe <pdhepe@xpanxion.com>
2021-02-26 17:38:59 +0530

    angular element poc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, Output } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-popup',
+  template: `
+    <span>Popup: {{message}}</span>
+    <button (click)="handleClick()" style="width: 200px; height: 50px;">{{message}}</button>
+  `, 
+  styles: [],
 })
 export class AppComponent {
-  title = 'custom-element';
+   private numberOfClicks = 0;
+
+  @Input()
+  get message(): string { return this._message; }
+  set message(message: string) {
+    this._message = message;  
+  }
+  private _message: string;
+
+  handleClick() {
+    this.numberOfClicks++;
+    this.message = `message _ ${this.numberOfClicks}`;
+    //this.action.emit(this.numberOfClicks);
+  }
+
+  //@Output()
+  //closed = new EventEmitter();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
+ 
+  imports: [
+    BrowserModule, BrowserAnimationsModule
+  ],
+  providers: [],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('popup-element', myElement);
+  }
+  ngDoBootstrap() {}
+}
